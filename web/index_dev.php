d9fb240ae289487b54fbcaf227d497cde74d396d
Arnaud Baali <arnaud.baali@me.com>
2016-11-28 16:18:00 +0000

    Add fabpot silex skeleton to the project

--- web/index_dev.php
+++ web/index_dev.php
@@ -2,13 +2,11 @@
 
 use Symfony\Component\Debug\Debug;
 
-ini_set('display_errors', 0);
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 Debug::enable();
 
 $app = require __DIR__.'/../src/app.php';
-require __DIR__.'/../config/prod.php';
+require __DIR__.'/../config/dev.php';
 require __DIR__.'/../src/controllers.php';
 $app->run();
